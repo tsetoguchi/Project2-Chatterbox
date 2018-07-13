7d5c6f7f4c21267f81e6e914715b803eb28fdfb6
Tao Setoguchi <tao209@gmail.com>
2018-07-13 05:23:20 +0000

    first commit

---
+++ env.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
+export DATABASE_URL=postgres://rtzgjsvvkpeusd:aa2a2d9c792b737d72e274aa5995a8fa322079966c186560a3e074d3896bd217@ec2-54-83-11-247.compute-1.amazonaws.com:5432/d7lmc0jhphip5p
